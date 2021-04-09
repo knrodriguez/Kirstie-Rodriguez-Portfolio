e17b8efc2a1cdd90ee578c51c3d6bf8089b7d53e
K <KirstieNRodriguez@gmail.com>
2021-04-08 22:59:23 -0400

    feat: adds pulse animation, star image, styling

--- src/index.js
+++ src/index.js
@@ -15,6 +15,32 @@ function selectPage(pageName) {
     }
 }
 
-function openModal(name){
-    console.log('HIYA', name)
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+
+function openModal(id){
+    console.log(id);
+    const modal = document.getElementById('modal');
+    modal.setAttribute('background-color','white');
+    modal.setAttribute('width', '500px')
+    modal.setAttribute('height', '500px')
+    modal.setAttribute("display", "block");
+    const h1 = document.createElement('h1');
+    h1.innerText = 'HELLOOOOOOO';
+    modal.appendChild(h1);
+    console.log(modal);
+
+
+
+    // const elem = document.getElementById(id);
+    // console.log(elem)
+    // const div = document.createElement('div');
+    // const h1 = document.createElement('h1');
+    // h1.innerText = 'HELLOOOOOOO';
+    // div.appendChild(h1);
+    // div.setAttribute('background-color','white');
+    // div.setAttribute('width', '500px')
+    // div.setAttribute('height', '500px')
+    // insertAfter(div, elem);
 }
